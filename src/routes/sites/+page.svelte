8d33a33f0568994f15b5adaa4bb0be5ba1ad4002
Kelly <kelliebrighty@gmail.com>
2026-02-26 02:58:51 +0100

    added search box
    Signed-off-by: Kelly <kelliebrighty@gmail.com>

--- src/routes/sites/+page.svelte
+++ src/routes/sites/+page.svelte
@@ -82,7 +82,7 @@
 			name="search"
 			id="search"
 			class="pl-10 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-			placeholder="Search by name, domain, or repo..."
+			placeholder="Search by site name, domain, or repo..."
 			bind:value={searchQuery}
 		/>
 	</div>
