82cf4aa487269494654183705a6529602514a4eb
Kelly <kelliebrighty@gmail.com>
2026-02-27 09:09:37 +0100

    feat: switch from adapter-static to adapter-node for SSR API routes

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: 'index.html', // This turns the app into a true Single Page App (SPA)
-			pages: 'build',
-			assets: 'build',
-			precompress: false,
-			strict: true
+			out: 'build'
 		})
 	}
 };
